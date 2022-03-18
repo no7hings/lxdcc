181642e9dd37db378f1333878e6705263684e2f9
dongchangbao <dongchangbao@papegames.net>
2022-03-18 16:32:59 +0800

    common update

--- script/python/lxsession/.test/_tst__maya_rig_export.py
+++ script/python/lxsession/.test/_tst__maya_rig_export.py
@@ -12,7 +12,19 @@ utl_core.Environ.set_add(
 user = bsc_core.SystemMtd.get_user_name()
 
 for i_file_path in [
-    '/l/prod/cgm/work/assets/chr/ext_andy/rig/rigging/maya/scenes/ext_andy.rig.rigging.v001.ma'
+    # '/l/prod/cgm/work/assets/chr/ext_andy/rig/rigging/maya/scenes/ext_andy.rig.rigging.v001.ma',
+    # '/l/prod/cgm/work/assets/chr/ext_ben/rig/rigging/maya/scenes/ext_ben.rig.rigging.v001.ma',
+    '/l/prod/cgm/work/assets/chr/ext_burk/rig/rigging/maya/scenes/ext_burk.rig.rigging.v001.ma',
+    '/l/prod/cgm/work/assets/chr/ext_erzi/rig/rigging/maya/scenes/ext_erzi.rig.rigging.v001.ma',
+    '/l/prod/cgm/work/assets/chr/ext_fuzzy/rig/rigging/maya/scenes/ext_fuzzy.rig.rigging.v001.ma',
+    '/l/prod/cgm/work/assets/chr/ext_johnny/rig/rigging/maya/scenes/ext_johnny.rig.rigging.v001.ma',
+    '/l/prod/cgm/work/assets/chr/ext_luo/rig/rigging/maya/scenes/ext_luo.rig.rigging.v001.ma',
+    '/l/prod/cgm/work/assets/chr/ext_mannequin/rig/rigging/maya/scenes/ext_mannequin.rig.rigging.v001.ma',
+    '/l/prod/cgm/work/assets/chr/ext_mike/rig/rigging/maya/scenes/ext_mike.rig.rigging.v001.ma',
+    '/l/prod/cgm/work/assets/chr/ext_owl_a/rig/rigging/maya/scenes/ext_owl_a.rig.rigging.v001.ma',
+    '/l/prod/cgm/work/assets/chr/ext_proketeriat/rig/rigging/maya/scenes/ext_proketeriat.rig.rigging.v001.ma',
+    '/l/prod/cgm/work/assets/chr/ext_suraj/rig/rigging/maya/scenes/ext_suraj.rig.rigging.v001.ma',
+    '/l/prod/cgm/work/assets/chr/ext_woodpecker/rig/rigging/maya/scenes/ext_woodpecker.rig.rigging.v001.ma'
 ]:
     j_option_opt = bsc_core.KeywordArgumentsOpt(
         option=dict(
@@ -21,8 +33,8 @@ for i_file_path in [
             file=i_file_path,
             user=bsc_core.SystemMtd.get_user_name(),
             #
-            td_enable=True,
-            # rez_beta=True,
+            # td_enable=True,
+            rez_beta=True,
         )
     )
     #
